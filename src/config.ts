f21b04ed8c8527bcb89c8b04bf89964b8683b8ad
sprucy <45484048+sprucy@users.noreply.github.com>
2025-04-08 22:05:34 +0200

    Update config.ts for comments

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-// API配置
+// API Configuration
 export const API_CONFIG = {
     BASE_URL: 'https://uk.api.just-eat.io',
     DISCOVERY_ENDPOINT: '/discovery/uk/restaurants/enriched/bypostcode/'    
